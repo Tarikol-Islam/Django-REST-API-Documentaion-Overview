3c1a98e22add85cf68dac3292b434a814580a488
Mohammad Tarikol Islam <tuhidtitw98@gmail.com>
2021-07-24 00:43:47 +0600

    First Commit

---
+++ snippets/views.py
@@ -0,0 +1,43 @@
+from snippets.permission import IsOwnerOrReadOnly
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics,permissions
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
+from rest_framework.decorators import api_view,action
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from rest_framework import renderers, viewsets
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
